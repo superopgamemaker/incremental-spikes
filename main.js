f3a18e0621f2e76fb5d76e39377e5390edf11312
inflationmaker <56657974+superopgamemaker@users.noreply.github.com>
2020-02-15 12:42:45 -0800

    or this....

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var balls = 0;
 
 function getballs(){
   balls++
-  document.getElementById("display").innerHTML = "You have " ++ balls ++ " balls."
+  document.getElementById("display").innerHTML = "You have ' ++ balls ++ ' balls."
 }
